c5e90ea89ed7807f74df49f197938ea262d38176
Damian Raczek <draczekprojekt@gmail.com>
2024-10-11 09:44:41 +0200

    code-behind implemented

--- App2/App2/MainPage.xaml.cs
+++ App2/App2/MainPage.xaml.cs
@@ -10,9 +10,28 @@ namespace App2
 {
     public partial class MainPage : ContentPage
     {
+        private string[] quotes = { "Dzień dobry", "Good morning", "Buenos dias" };
+        private int currentQuoteIndex = 0;
+
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int fontSize = (int)e.NewValue;
+            quoteLabel.FontSize = fontSize;
+
+            // Ustawiamy wartość na etykiecie "Rozmiar:"
+            fontSizeLabel.Text = $"Rozmiar: {fontSize}";
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            currentQuoteIndex = (currentQuoteIndex + 1) % quotes.Length;
+            quoteLabel.Text = quotes[currentQuoteIndex];
         }
     }
 }
